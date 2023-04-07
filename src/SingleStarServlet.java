7c141479bf2ee0090b9bc1c2d2382fa156d60d0d
Vanessa Tang <vanesst5@uci.edu>
2023-04-06 23:32:32 -0700

    Made some simple changes

--- src/SingleStarServlet.java
+++ src/SingleStarServlet.java
@@ -19,7 +19,7 @@ import java.sql.ResultSet;
 // Declaring a WebServlet called SingleStarServlet, which maps to url "/api/single-star"
 @WebServlet(name = "SingleStarServlet", urlPatterns = "/api/single-star")
 public class SingleStarServlet extends HttpServlet {
-    private static final long serialVersionUID = 2L;
+    private static final long serialVersionUID = 3L;
 
     // Create a dataSource which registered in web.xml
     private DataSource dataSource;
