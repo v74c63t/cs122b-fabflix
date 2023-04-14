6f254c286bcf4b22ce62b2ca74fb2d35739cf449
Vanessa Tang <vanesst5@uci.edu>
2023-04-14 16:43:32 -0700

    Removed unused code/comments

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -14,7 +14,6 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginServlet extends HttpServlet {
@@ -45,9 +44,6 @@ public class LoginServlet extends HttpServlet {
         // Output stream to STDOUT
         PrintWriter out = response.getWriter();
 
-        /* This example only allows username/password to be test/test
-        /  in the real project, you should talk to the database to verify username/password
-        */
         JsonObject responseJsonObject = new JsonObject();
         // Get a connection from dataSource and let resource manager close the connection after usage.
         try (Connection conn = dataSource.getConnection()) {
@@ -70,9 +66,6 @@ public class LoginServlet extends HttpServlet {
             // Perform the query
             ResultSet rs = statement.executeQuery();
 
-//            JsonArray jsonArray = new JsonArray();
-
-            // Iterate through each row of rs
             if(rs.next()) {
                 // not sure whether to keep this information/add to user obj or not
 //                String customerId = rs.getString("id");
@@ -97,12 +90,7 @@ public class LoginServlet extends HttpServlet {
                 responseJsonObject.addProperty("status", "fail");
                 // Log to localhost log
                 request.getServletContext().log("Login failed");
-                // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
-//                if (!username.equals("a@email.com")) {
-//                    responseJsonObject.addProperty("message", "User (" + username + ") doesn't exist");
-//                } else {
-//                    responseJsonObject.addProperty("message", "Incorrect password");
-//                }
+
                 responseJsonObject.addProperty("message", "User doesn't exist or incorrect password");
             }
         rs.close();
