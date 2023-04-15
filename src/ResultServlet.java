d8788819461ed512c4ca6f8f519b0f15176402b2
Haver Ho <haverho.2020@gmail.com>
2023-04-15 11:28:07 -0700

    commented out things and testing methods to get paramters

--- src/ResultServlet.java
+++ src/ResultServlet.java
@@ -1,3 +1,4 @@
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
@@ -14,6 +15,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Arrays;
 import java.util.Map;
 
 // Declaring a WebServlet called SingleStarServlet, which maps to url "/api/single-star"
@@ -42,11 +44,12 @@ public class ResultServlet extends HttpServlet {
         response.setContentType("application/json"); // Response mime type
 
         // Retrieve parameter id from url request.
+        // Testing out Servlet functions
+        String genreId = request.getParameter("genreId");
         Map<String, String[]> paramMap = request.getParameterMap();
-        System.out.println(paramMap);
 
         // The log message can be found in localhost log
-        request.getServletContext().log("parameter maps: " + paramMap);
+        request.getServletContext().log("getting genreId: " + genreId);
 
         // Output stream to STDOUT
         PrintWriter out = response.getWriter();
@@ -56,15 +59,21 @@ public class ResultServlet extends HttpServlet {
             // Get a connection from dataSource
 
             // Construct a query with parameter represented by "?"
-            String query = "SELECT * from stars as s, stars_in_movies as sim, movies as m " +
-                    "where m.id = sim.movieId and sim.starId = s.id and s.id = ?";
+            // REPLACE LATER || ONLY TESTING genres
+            String query = "select gim.movieId, title, year, director, rating " +
+                    "from genres_in_movies as gim " +
+                    "join movies as m " +
+                    "join ratings as r " +
+                    "on r.movieId = m.id " +
+                    "and m.id = gim.movieId " +
+                    "where genreId = ?";
 
             // Declare our statement
             PreparedStatement statement = conn.prepareStatement(query);
 
             // Set the parameter represented by "?" in the query to the id we get from url,
             // num 1 indicates the first "?" in the query
-            statement.setString(1, id);
+            statement.setString(1, genreId);
 
             // Perform the query
             ResultSet rs = statement.executeQuery();
@@ -72,28 +81,37 @@ public class ResultServlet extends HttpServlet {
             JsonArray jsonArray = new JsonArray();
 
             // Iterate through each row of rs
-            while (rs.next()) {
-
-                String starId = rs.getString("starId");
-                String starName = rs.getString("name");
-                String starDob = rs.getString("birthYear");
-
-                String movieId = rs.getString("movieId");
-                String movieTitle = rs.getString("title");
-                String movieYear = rs.getString("year");
-                String movieDirector = rs.getString("director");
-
+//            while (rs.next()) {
+//
+//                String starId = rs.getString("starId");
+//                String starName = rs.getString("name");
+//                String starDob = rs.getString("birthYear");
+//
+//                String movieId = rs.getString("movieId");
+//                String movieTitle = rs.getString("title");
+//                String movieYear = rs.getString("year");
+//                String movieDirector = rs.getString("director");
+//                String movieRating = rs.getString("rating");
+//
                 // Create a JsonObject based on the data we retrieve from rs
+//
+//                JsonObject jsonObject = new JsonObject();
+//                jsonObject.addProperty("star_id", starId);
+//                jsonObject.addProperty("star_name", starName);
+//                jsonObject.addProperty("star_dob", starDob);
+//                jsonObject.addProperty("movie_id", movieId);
+//                jsonObject.addProperty("movie_title", movieTitle);
+//                jsonObject.addProperty("movie_year", movieYear);
+//                jsonObject.addProperty("movie_director", movieDirector);
+//                jsonObject.addProperty("movie_rating", movieRating);
+//
+//                jsonArray.add(jsonObject);
+//            }
 
+            // TESTING TO GET ALL PARAMTERS of URL
+            for (Map.Entry<String, String[]> entry : paramMap.entrySet()) {
                 JsonObject jsonObject = new JsonObject();
-                jsonObject.addProperty("star_id", starId);
-                jsonObject.addProperty("star_name", starName);
-                jsonObject.addProperty("star_dob", starDob);
-                jsonObject.addProperty("movie_id", movieId);
-                jsonObject.addProperty("movie_title", movieTitle);
-                jsonObject.addProperty("movie_year", movieYear);
-                jsonObject.addProperty("movie_director", movieDirector);
-
+                jsonObject.addProperty(entry.getKey(), Arrays.toString(entry.getValue()));
                 jsonArray.add(jsonObject);
             }
             rs.close();
@@ -123,3 +141,4 @@ public class ResultServlet extends HttpServlet {
     }
 
 }
+
