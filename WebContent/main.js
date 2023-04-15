83173d233138bacb7f774923b18873a75e018200
Vanessa Tang <vanesst5@uci.edu>
2023-04-14 20:07:51 -0700

    Added in alpha/num list

--- WebContent/main.js
+++ WebContent/main.js
@@ -18,9 +18,33 @@ function handleGenreResult(resultData) {
     }
 }
 
+function handleTitle() {
+    console.log('handling title');
+    let alphaList = jQuery("#alpha-list");
+    let alpha = 'A';
+    for(let i = 0; i < 26; i++){
+        let alphaHTML = '<h4>' + alpha + '</h4>';
+        alpha = String.fromCharCode(alpha.charCodeAt(0) + 1)
+        alphaList.append(alphaHTML);
+    }
+    let numList = jQuery("#num-list");
+    let num = '0';
+    for(let i = 0; i < 10; i++){
+        let numHTML = '<h4>' + num + '</h4>';
+        num = String.fromCharCode(num.charCodeAt(0) + 1)
+        numList.append(numHTML);
+    }
+    let numHTML = '<h4>*</h4>';
+    numList.append(numHTML);
+}
+function handleInit(resultData) {
+    handleGenreResult(resultData);
+    handleTitle();
+}
+
 jQuery.ajax({
     dataType: "json", // Setting return data type
     method: "GET", // Setting request method
     url: "api/maininit", // Setting request url, which is mapped by MoviesServlet
-    success: (resultData) => handleGenreResult(resultData) // Setting callback function to handle data returned successfully by the MoviesServlet
+    success: (resultData) => handleInit(resultData) // Setting callback function to handle data returned successfully by the MoviesServlet
 });
