9ae26998b56047068abc075bb06a9b5b5c58d4ef
Vanessa Tang <vanesst5@uci.edu>
2023-04-14 21:15:12 -0700

    Changed link colors

--- WebContent/main.js
+++ WebContent/main.js
@@ -14,7 +14,7 @@ function handleGenreResult(resultData) {
     for (let i = 0; i < resultData.length; i++) {
         let genre = resultData[i]["genre_name"];
         let genreId = resultData[i]['genre_id'];
-        let listHTML = "<h4><a href='result.html?genreId=" + genreId + "'>" + genre + "</a></h4>";
+        let listHTML = "<h4><a style='color: #ffc107;' href='result.html?genreId=" + genreId + "'>" + genre + "</a></h4>";
         // add in the href later
         genreList.append(listHTML);
     }
@@ -25,18 +25,18 @@ function handleTitle() {
     let alphaList = jQuery("#alpha-list");
     let alpha = 'A';
     for(let i = 0; i < 26; i++){
-        let alphaHTML = "<h4><a href='result.html?startTitle=" + alpha + "'>" + alpha + '</a></h4>';
+        let alphaHTML = "<h4><a style='color: #ffc107;' href='result.html?startTitle=" + alpha + "'>" + alpha + '</a></h4>';
         alpha = String.fromCharCode(alpha.charCodeAt(0) + 1)
         alphaList.append(alphaHTML);
     }
     let numList = jQuery("#num-list");
     let num = '0';
     for(let i = 0; i < 10; i++){
-        let numHTML = "<h4><a href='result.html?startTitle=" + num + "'>" + num + '</a></h4>';
+        let numHTML = "<h4><a style='color: #ffc107;' href='result.html?startTitle=" + num + "'>" + num + '</a></h4>';
         num = String.fromCharCode(num.charCodeAt(0) + 1)
         numList.append(numHTML);
     }
-    let numHTML = "<h4><a href='result.html?startTitle=*'>*</a></h4>";
+    let numHTML = "<h4><a style='color: #ffc107;' href='result.html?startTitle=*'>*</a></h4>";
     numList.append(numHTML);
 }
 function handleInit(resultData) {
