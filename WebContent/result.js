7a531938ca23fef9198469e4576dadbbe4594c31
Haver Ho <haverho.2020@gmail.com>
2023-04-18 10:10:57 -0700

    modified result.js so it knows which Servlet to request from

--- WebContent/result.js
+++ WebContent/result.js
@@ -33,40 +33,29 @@ function handleResult(resultData) {
     console.log("handleResult: movie info from resultData");
     console.log(resultData)
 
-    // let movieTitle = jQuery("#movies_table_body");
-    // movieTitle.append(resultData[0]["movie_title"]);
-    // let movieInfoElement = jQuery("#movie_info");
-    //
-    // movieInfoElement.append("<div style='padding-left: 80px;'><i><strong>" + resultData[0]["movie_title"] + "</strong></i><span class='page-title-sub-info'> (" +
-    //     + resultData[0]["movie_year"]+ ")</span></div>");
-    //
-    // console.log("handleResult: populating star table from resultData");
-    //
-    // // Populate table
-    // let movieTableBodyElement = jQuery("#single_movie_table_body");
-    //
-    // // Concatenate the html tags with resultData jsonObject to create table rows
-    // for (let i = 0; i < resultData.length; i++) {
-    //     let starsArray = resultData[i]["movie_stars"].split(", ");
-    //     let rowHTML = "";
-    //     rowHTML += "<tr>";
-    //     rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
-    //     rowHTML += "<th>" + resultData[i]["movie_genres"] + "</th>";
-    //     rowHTML += "<th>";
-    //     for (let stars in starsArray) {
-    //         let starsArr = starsArray[stars].split("|");
-    //         rowHTML += "<a style='color:darkturquoise;' href='single-star.html?id=" + starsArr[0]
-    //             + "'>" + starsArr[1] + "</a>" + ", ";
-    //     }
-    //     rowHTML = rowHTML.substring(0,rowHTML.length-3);
-    //     rowHTML += "</th>";
-    //     rowHTML += "<th>" + resultData[i]["movie_rating"] +
-    //         " <i class='fa-sharp fa-solid fa-star' style='color: #ffd747;'></i></th>";
-    //     rowHTML += "</tr>";
-    //
-    //     // Append the row created to the table body, which will refresh the page
-    //     movieTableBodyElement.append(rowHTML);
-    // }
+    let movieTableBodyElement = jQuery("#movies_table_body");
+    // Concatenate the html tags with resultData jsonObject to create table rows
+    for (let i = 0; i < resultData.length; i++) {
+        let starsArray = resultData[i]["movie_stars"].split(", ");
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_genres"] + "</th>";
+        rowHTML += "<th>";
+        for (let stars in starsArray) {
+            let starsArr = starsArray[stars].split("|");
+            rowHTML += "<a style='color:darkturquoise;' href='single-star.html?id=" + starsArr[0]
+                + "'>" + starsArr[1] + "</a>" + ", ";
+        }
+        rowHTML = rowHTML.substring(0,rowHTML.length-3);
+        rowHTML += "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_rating"] +
+            " <i class='fa-sharp fa-solid fa-star' style='color: #ffd747;'></i></th>";
+        rowHTML += "</tr>";
+
+        // Append the row created to the table body, which will refresh the page
+        movieTableBodyElement.append(rowHTML);
+    }
 }
 
 
@@ -77,10 +66,13 @@ function handleResult(resultData) {
 
 // Get id from URL
 let urlRequest = '';
+let servletUrl = '';
 if ( getParameterByName('genreId') ) {
     urlRequest = 'genreId=' + getParameterByName('genreId')
+    servletUrl += 'api/by-genre?'
 }else if ( getParameterByName('startTitle') ) {
     urlRequest = 'startTitle=' + getParameterByName('startTitle')
+    servletUrl += 'api/by-start-title?'
 }else {
     let obj = {}
     if ( getParameterByName('title') ) { obj['title'] = getParameterByName('title') }
@@ -88,12 +80,13 @@ if ( getParameterByName('genreId') ) {
     if ( getParameterByName('year') ) { obj['year'] = getParameterByName('year') }
     if ( getParameterByName('star') ) { obj['star'] = getParameterByName('star') }
     urlRequest = getURLParams(obj);
+    servletUrl += 'api/by-search?'
 }
 
 // Makes the HTTP GET request and registers on success callback function handleResult
 jQuery.ajax({
     dataType: "json",  // Setting return data type
     method: "GET",// Setting request method
-    url: "api/result?" + urlRequest, // Setting request url, which is mapped by SingleMovieServlet
+    url: servletUrl + urlRequest, // Setting request url, which is mapped by SingleMovieServlet
     success: (resultData) => handleResult(resultData) // Setting callback function to handle data returned successfully by the SingleMovieServlet
 });
