55493e2713b1aebac5a2cd58f1ee459f818e863c
Vanessa Tang <vanesst5@uci.edu>
2023-04-27 20:40:47 -0700

    Added in check for first/last name of credit card holder

--- src/PaymentServlet.java
+++ src/PaymentServlet.java
@@ -46,65 +46,77 @@ public class PaymentServlet extends HttpServlet {
         System.out.println(request.getParameter("last"));
         // Output stream to STDOUT
         PrintWriter out = response.getWriter();
-
-
-        /* This example only allows username/password to be test/test
-        /  in the real project, you should talk to the database to verify username/password
-        */
         JsonObject responseJsonObject = new JsonObject();
-        try (Connection conn = dataSource.getConnection()) {
-            // Get a connection from dataSource
+        User customer = (User) request.getSession().getAttribute("user");
 
-            // Construct a query with parameter represented by "?"
-            String query = String.join("",
-                    "SELECT *",
-                    "FROM creditcards ",
-                    "WHERE id=? AND firstName=? AND lastName=? AND expirationDate=? ");
-
-            // Declare our statement
-            PreparedStatement statement = conn.prepareStatement(query);
-
-            // Set the parameter represented by "?" in the query to the id we get from url,
-            // num 1 indicates the first "?" in the query
-            statement.setString(1, ccId);
-            statement.setString(2, firstName);
-            statement.setString(3, lastName);
-            statement.setString(4, expirationDate);
-
-            // Perform the query
-            ResultSet rs = statement.executeQuery();
-
-            if(rs.next()) {
-                responseJsonObject.addProperty("status", "success");
-                responseJsonObject.addProperty("message", "success");
-            }
-            else {
-                responseJsonObject.addProperty("status", "fail");
-                // Log to localhost log
-                request.getServletContext().log("Verifying Failed");
-                responseJsonObject.addProperty("message", "Invalid credit card information");
-
-            }
-            rs.close();
-            statement.close();
-
+        if(customer.getFirstName().equals(firstName) || customer.getLastName().equals(lastName)){
+            responseJsonObject.addProperty("status", "fail");
+            // Log to localhost log
+            request.getServletContext().log("Verifying Failed");
+            responseJsonObject.addProperty("message", "Invalid credit card information");
             // Write JSON string to output
             response.getWriter().write(responseJsonObject.toString());
             // Set response status to 200 (OK)
             response.setStatus(200);
+        }
 
-        } catch (Exception e) {
-            // Write error message JSON object to output
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("errorMessage", e.getMessage());
-            out.write(jsonObject.toString());
+        /* This example only allows username/password to be test/test
+        /  in the real project, you should talk to the database to verify username/password
+        */
+        else {
+            try (Connection conn = dataSource.getConnection()) {
+                // Get a connection from dataSource
 
-            // Log error to localhost log
-            request.getServletContext().log("Error:", e);
-            // Set response status to 500 (Internal Server Error)
-            response.setStatus(500);
-        } finally {
-            out.close();
+                // Construct a query with parameter represented by "?"
+                String query = String.join("",
+                        "SELECT *",
+                        "FROM creditcards ",
+                        "WHERE id=? AND firstName=? AND lastName=? AND expirationDate=? ");
+
+                // Declare our statement
+                PreparedStatement statement = conn.prepareStatement(query);
+
+                // Set the parameter represented by "?" in the query to the id we get from url,
+                // num 1 indicates the first "?" in the query
+                statement.setString(1, ccId);
+                statement.setString(2, firstName);
+                statement.setString(3, lastName);
+                statement.setString(4, expirationDate);
+
+                // Perform the query
+                ResultSet rs = statement.executeQuery();
+
+                if (rs.next()) {
+                    responseJsonObject.addProperty("status", "success");
+                    responseJsonObject.addProperty("message", "success");
+                } else {
+                    responseJsonObject.addProperty("status", "fail");
+                    // Log to localhost log
+                    request.getServletContext().log("Verifying Failed");
+                    responseJsonObject.addProperty("message", "Invalid credit card information");
+
+                }
+                rs.close();
+                statement.close();
+
+                // Write JSON string to output
+                response.getWriter().write(responseJsonObject.toString());
+                // Set response status to 200 (OK)
+                response.setStatus(200);
+
+            } catch (Exception e) {
+                // Write error message JSON object to output
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("errorMessage", e.getMessage());
+                out.write(jsonObject.toString());
+
+                // Log error to localhost log
+                request.getServletContext().log("Error:", e);
+                // Set response status to 500 (Internal Server Error)
+                response.setStatus(500);
+            } finally {
+                out.close();
+            }
         }
     }
 }
