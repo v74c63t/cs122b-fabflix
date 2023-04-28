3c830876fb724b4e677407b37bc722c5831914f4
Vanessa Tang <vanesst5@uci.edu>
2023-04-27 19:57:21 -0700

    Checks if credit card info is valid and returns a message

--- WebContent/payment.js
+++ WebContent/payment.js
@@ -1,42 +1,53 @@
-// let payment_form = $("payment-form");
-
-// function handlePaymentResult(resultDataString) {
-//     let resultDataJson = JSON.parse(JSON.stringify(resultDataString));
-//
-//     console.log("handle login response");
-//     console.log(resultDataJson);
-//     console.log(resultDataJson["status"]);
-//
-//     // If login succeeds, it will redirect the user to index.html
-//     if (resultDataJson["status"] === "success") {
-//         // window.location.replace("main.html");
-//     } else {
-//         // If login fails, the web page will display
-//         // error messages on <div> with id "login_error_message"
-//         console.log("show error message");
-//         console.log(resultDataJson["message"]);
-//         $("#login_error_message").text(resultDataJson["message"]);
-//     }
-// }
 
 
-// function submitPaymentForm(formSubmitEvent) {
-//     console.log("submit payment form");
-//     /**
-//      * When users click the submit button, the browser will not direct
-//      * users to the url defined in HTML form. Instead, it will call this
-//      * event handler when the event is triggered.
-//      */
-//     formSubmitEvent.preventDefault();
-//
-//     $.ajax(
-//         "api/payment", {
-//             method: "POST",
-//             // Serialize the login form to the data sent by POST request
-//             data: login_form.serialize(),
-//             success: handlePaymentResult
-//         }
-//     );
-// }
+let payment_form = $("#payment_form");
 
-// payment_form.submit(submitPaymentForm());
+/**
+ * Handle the data returned by LoginServlet
+ * @param resultDataString jsonObject
+ */
+function handlePaymentResult(resultDataString) {
+    let resultDataJson = JSON.parse(resultDataString);
+
+    console.log("handle login response");
+    console.log(resultDataJson);
+    console.log(resultDataJson["status"]);
+
+    // If login succeeds, it will redirect the user to index.html
+    if (resultDataJson["status"] === "success") {
+        alert(resultDataJson["message"])
+        // window.location.replace("index.html");
+    } else {
+        // If login fails, the web page will display
+        // error messages on <div> with id "login_error_message"
+        console.log("show error message");
+        console.log(resultDataJson["message"]);
+        $("#payment_error_message").text(resultDataJson["message"]);
+    }
+}
+
+/**
+ * Submit the form content with POST method
+ * @param formSubmitEvent
+ */
+function submitPaymentForm(formSubmitEvent) {
+    console.log("submit login form");
+    /**
+     * When users click the submit button, the browser will not direct
+     * users to the url defined in HTML form. Instead, it will call this
+     * event handler when the event is triggered.
+     */
+    formSubmitEvent.preventDefault();
+
+    $.ajax(
+        "api/payment", {
+            method: "POST",
+            // Serialize the login form to the data sent by POST request
+            data: payment_form.serialize(),
+            success: handlePaymentResult
+        }
+    );
+}
+
+// Bind the submit action of the form to a handler function
+payment_form.submit(submitPaymentForm);
--- src/PaymentServlet.java
+++ src/PaymentServlet.java
@@ -1,27 +1,25 @@
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+
+import com.google.gson.JsonObject;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
 
 @WebServlet(name = "PaymentServlet", urlPatterns = "/api/payment")
 public class PaymentServlet extends HttpServlet {
-    private static final long serialVersionUID = 4L;
+
+    private static final long serialVersionUID = 11L;
 
     // Create a dataSource which registered in web.xml
     private DataSource dataSource;
@@ -33,121 +31,80 @@ public class PaymentServlet extends HttpServlet {
             e.printStackTrace();
         }
     }
-
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        String email= request.getParameter("email");
-//        String password = request.getParameter("password");
-//        response.setContentType("application/json"); // Response mime type
-//        // The log message can be found in localhost log
-//        request.getServletContext().log("getting email: " + email);
-//        request.getServletContext().log("getting password: " + password);
-//
-//        // Output stream to STDOUT
-//        PrintWriter out = response.getWriter();
-//
-//        JsonObject responseJsonObject = new JsonObject();
-//        // Get a connection from dataSource and let resource manager close the connection after usage.
-//        try (Connection conn = dataSource.getConnection()) {
-//            // Get a connection from dataSource
-//
-//            // Construct a query with parameter represented by "?"
-//            String query = String.join("",
-//                    "SELECT *",
-//                    "FROM customers ",
-//                    "WHERE email=? AND password=?");
-//
-//            // Declare our statement
-//            PreparedStatement statement = conn.prepareStatement(query);
-//
-//            // Set the parameter represented by "?" in the query to the id we get from url,
-//            // num 1 indicates the first "?" in the query
-//            statement.setString(1, email);
-//            statement.setString(2, password);
-//
-//            // Perform the query
-//            ResultSet rs = statement.executeQuery();
-//
-//            if(rs.next()) {
-////                while(rs.next()) {
-//                // not sure whether to keep this information/add to user obj or not
-////                String customerId = rs.getString("id");
-////                String customerFirstName = rs.getString("firstName");
-////                String customerLastName = rs.getString("lastName");
-////                String customerCcId = rs.getString("ccId");
-////                String customerAddress = rs.getString("address");
-////
-////                // Create a JsonObject based on the data we retrieve from rs
-////                responseJsonObject.addProperty("customer_id", customerId);
-////                responseJsonObject.addProperty("customer_first_name", customerFirstName);
-////                responseJsonObject.addProperty("customer_last_name", customerLastName);
-////                responseJsonObject.addProperty("customer_cc_id", customerCcId);
-////                responseJsonObject.addProperty("customer_address", customerAddress);
-////                    if(rs.getString("password").equals(password)) {
-//                request.getSession().setAttribute("user", new User(email));
-//                responseJsonObject.addProperty("status", "success");
-//                responseJsonObject.addProperty("message", "success");
-////                        break;
-////                    }
-////                }
-////                if (request.getSession().getAttribute("user") == null) {
-////                    // Login fail
-////                    responseJsonObject.addProperty("status", "fail");
-////                    // Log to localhost log
-////                    request.getServletContext().log("Login failed");
-////
-////                    responseJsonObject.addProperty("message", "Incorrect password");
-////                }
-//            }
-//            else {
-//                String query2 = String.join("",
-//                        "SELECT *",
-//                        "FROM customers ",
-//                        "WHERE email=? ");
-//                PreparedStatement statement2 = conn.prepareStatement(query2);
-//                statement2.setString(1, email);
-//                ResultSet rs2 = statement2.executeQuery();
-//                if(rs2.next()) {
-//                    // Login fail
-//                    responseJsonObject.addProperty("status", "fail");
-//                    // Log to localhost log
-//                    request.getServletContext().log("Login failed");
-//
-//                    responseJsonObject.addProperty("message", "Incorrect password");
-//                }
-//                else {
-//                    // Login fail
-//                    responseJsonObject.addProperty("status", "fail");
-//                    // Log to localhost log
-//                    request.getServletContext().log("Login failed");
-//
-//                    responseJsonObject.addProperty("message", "User with that email doesn't exist");
-//                }
-//                rs2.close();
-//                statement2.close();
-//            }
-//            rs.close();
-//            statement.close();
-//
-//            // Write JSON string to output
-//            response.getWriter().write(responseJsonObject.toString());
-//            // Set response status to 200 (OK)
-//            response.setStatus(200);
-//
-//        } catch (Exception e) {
-//            // Write error message JSON object to output
-//            JsonObject jsonObject = new JsonObject();
-//            jsonObject.addProperty("errorMessage", e.getMessage());
-//            out.write(jsonObject.toString());
-//
-//            // Log error to localhost log
-//            request.getServletContext().log("Error:", e);
-//            // Set response status to 500 (Internal Server Error)
-//            response.setStatus(500);
-//        } finally {
-//            out.close();
-//        }
+        String ccId = request.getParameter("ccId");
+        String firstName = request.getParameter("first");
+        String lastName = request.getParameter("last");
+
+        String expirationDate = request.getParameter("expirationDate");
+        System.out.println(request.getParameter("expirationDate"));
+        System.out.println(request.getParameter("ccId"));
+        System.out.println(request.getParameter("first"));
+        System.out.println(request.getParameter("last"));
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+
+        /* This example only allows username/password to be test/test
+        /  in the real project, you should talk to the database to verify username/password
+        */
+        JsonObject responseJsonObject = new JsonObject();
+        try (Connection conn = dataSource.getConnection()) {
+            // Get a connection from dataSource
+
+            // Construct a query with parameter represented by "?"
+            String query = String.join("",
+                    "SELECT *",
+                    "FROM creditcards ",
+                    "WHERE id=? AND firstName=? AND lastName=? AND expirationDate=? ");
+
+            // Declare our statement
+            PreparedStatement statement = conn.prepareStatement(query);
+
+            // Set the parameter represented by "?" in the query to the id we get from url,
+            // num 1 indicates the first "?" in the query
+            statement.setString(1, ccId);
+            statement.setString(2, firstName);
+            statement.setString(3, lastName);
+            statement.setString(4, expirationDate);
+
+            // Perform the query
+            ResultSet rs = statement.executeQuery();
+
+            if(rs.next()) {
+                responseJsonObject.addProperty("status", "success");
+                responseJsonObject.addProperty("message", "success");
+            }
+            else {
+                responseJsonObject.addProperty("status", "fail");
+                // Log to localhost log
+                request.getServletContext().log("Verifying Failed");
+                responseJsonObject.addProperty("message", "Invalid credit card information");
+
+            }
+            rs.close();
+            statement.close();
+
+            // Write JSON string to output
+            response.getWriter().write(responseJsonObject.toString());
+            // Set response status to 200 (OK)
+            response.setStatus(200);
+
+        } catch (Exception e) {
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Log error to localhost log
+            request.getServletContext().log("Error:", e);
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
     }
 }
