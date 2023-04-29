a131d36e03363a2334d1aebcad65977d62b83891
Vanessa Tang <vanesst5@uci.edu>
2023-04-28 22:17:21 -0700

    Fixed error with total price

--- WebContent/cart.js
+++ WebContent/cart.js
@@ -42,27 +42,27 @@ function handleCartArray(resultData) {
                 let resultTab = jQuery("#result");
                 resultTab.attr("href", "result.html?" + resultData[0]["resultUrl"]);
             }
+            // console.log(resultData);
+            let totalPrice = 0;
+            for (let i = 0; i < resultData.length; i++) {
+                let rowHTML = "<tr>";
+                rowHTML += "<th>" +
+                    '<a style="color:darkturquoise;" href="single-movie.html?id=' + resultData[i]["movie_id"] + '">' +
+                    resultData[i]["movie_title"] +
+                    '</a>'+
+                    "</th>";
+                rowHTML += "<th><button type='submit' class='btn btn-secondary' style='margin-right:10px' onclick=\"handleCart("+ "'" + resultData[i]["movie_id"] + "'" + ", -1.0)\"><i class='fa-solid fa-minus'></i></button>" +
+                    resultData[i]['movie_quantity'] + "<button type='submit' class='btn btn-secondary' style='margin-left:10px' onclick=\"handleCart("+ "'" + resultData[i]["movie_id"] + "'" + ", 1.0)\"><i class='fa-solid fa-plus'></i></button>" +
+                    "</th>";
+                rowHTML += "<th><button type='submit' class='btn btn-outline-danger' onclick=\"handleCart("+ "'" + resultData[i]["movie_id"] + "'" + ", 0.0)\">Delete</button></th>";
+                rowHTML += "<th>$" + resultData[i]['movie_price'].toFixed(2) + "</th>";
+                totalPrice += parseFloat((parseInt(resultData[i]['movie_quantity']) * parseFloat(resultData[i]['movie_price'])).toFixed(2));
+                rowHTML += "<th>$" + (parseInt(resultData[i]['movie_quantity']) * parseFloat(resultData[i]['movie_price'])).toFixed(2).toString() + "</th>";
+                rowHTML += '</tr>';
+                cartTableBody.append(rowHTML);
+            }
+            document.getElementById("total").innerText = "Total Price: $" + totalPrice.toFixed(2);
         }
-        // console.log(resultData);
-        let totalPrice = 0;
-        for (let i = 0; i < resultData.length; i++) {
-            let rowHTML = "<tr>";
-            rowHTML += "<th>" +
-                '<a style="color:darkturquoise;" href="single-movie.html?id=' + resultData[i]["movie_id"] + '">' +
-                resultData[i]["movie_title"] +
-                '</a>'+
-                "</th>";
-            rowHTML += "<th><button type='submit' class='btn btn-secondary' style='margin-right:10px' onclick=\"handleCart("+ "'" + resultData[i]["movie_id"] + "'" + ", -1.0)\"><i class='fa-solid fa-minus'></i></button>" +
-                resultData[i]['movie_quantity'] + "<button type='submit' class='btn btn-secondary' style='margin-left:10px' onclick=\"handleCart("+ "'" + resultData[i]["movie_id"] + "'" + ", 1.0)\"><i class='fa-solid fa-plus'></i></button>" +
-                "</th>";
-            rowHTML += "<th><button type='submit' class='btn btn-outline-danger' onclick=\"handleCart("+ "'" + resultData[i]["movie_id"] + "'" + ", 0.0)\">Delete</button></th>";
-            rowHTML += "<th>$" + resultData[i]['movie_price'].toFixed(2) + "</th>";
-            totalPrice += parseFloat((parseInt(resultData[i]['movie_quantity']) * parseFloat(resultData[i]['movie_price'])).toFixed(2));
-            rowHTML += "<th>$" + (parseInt(resultData[i]['movie_quantity']) * parseFloat(resultData[i]['movie_price'])).toFixed(2).toString() + "</th>";
-            rowHTML += '</tr>';
-            cartTableBody.append(rowHTML);
-        }
-        document.getElementById("total").innerText = "Total Price: $" + totalPrice.toFixed(2);
     }
 }
 
