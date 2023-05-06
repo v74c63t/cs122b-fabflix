dc6babc71bbb581587517d60370fd61379b0bede
Haver Ho <haverho.2020@gmail.com>
2023-05-05 18:44:10 -0700

    changed path to adjust to directory change

--- WebContent/_dashboard/employees-login.js
+++ WebContent/_dashboard/employees-login.js
@@ -41,7 +41,7 @@ function submitLoginForm(formSubmitEvent) {
     formSubmitEvent.preventDefault();
 
     $.ajax(
-        "api/employee-login", {
+        "../api/employee-login", {
             method: "POST",
             // Serialize the login form to the data sent by POST request
             data: login_form.serialize(),
