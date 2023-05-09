42c1108a2667869d1d60679effd3b46924252594
Haver Ho <haverho.2020@gmail.com>
2023-05-09 14:18:49 -0700

    updated AddMovieServlet [ returns an jsonObject instead of jsonArray ]

--- src/AddMovieServlet.java
+++ src/AddMovieServlet.java
@@ -19,7 +19,95 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+// Declaring a WebServlet called SingleStarServlet, which maps to url "/api/single-star"
 @WebServlet(name = "AddMovieServlet", urlPatterns = "/api/add-movie")
 public class AddMovieServlet extends HttpServlet{
+    private static final long serialVersionUID = 3L;
+
+    // Create a dataSource which registered in web.xml
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Retrieve parameter id from url request.
+        String title = request.getParameter("title");
+        String year = request.getParameter("year");
+        String director = request.getParameter("director");
+        String star = request.getParameter("star");
+        String birth = request.getParameter("birth");
+        String genre = request.getParameter("genre");
+
+        // The log message can be found in localhost log
+        request.getServletContext().log("getting title: " + title);
+        request.getServletContext().log("getting year: " + year);
+        request.getServletContext().log("getting director: " + director);
+        request.getServletContext().log("getting star: " + star);
+        request.getServletContext().log("getting birth: " + birth);
+        request.getServletContext().log("getting genre: " + genre);
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        // Get a connection from dataSource and let resource manager close the connection after usage.
+        try (Connection conn = dataSource.getConnection()) {
+
+            // Construct a query with parameter represented by "?"
+            String query = "CALL add_movie(?, ?, ?, ? ,?, ?);";
+
+            PreparedStatement statement = conn.prepareStatement(query);
+
+            statement.setString(1, title);
+            statement.setInt(2, Integer.parseInt(year));
+            statement.setString(3, director);
+            statement.setString(4, star);
+            statement.setInt(5, Integer.parseInt(birth));
+            statement.setString(6, genre);
+
+            ResultSet rs = statement.executeQuery();
+
+            JsonObject jsonObject = new JsonObject();
+
+            while(rs.next()) {
+                jsonObject.addProperty("message", rs.getString("message"));
+            }
+            rs.close();
+            statement.close();
+
+            // Write JSON string to output
+            out.write(jsonObject.toString());
+            // Set response status to 200 (OK)
+            response.setStatus(200);
+
+        } catch (Exception e) {
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Log error to localhost log
+            request.getServletContext().log("Error:", e);
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+
+        // Always remember to close db connection after usage. Here it's done by try-with-resources
+
+    }
 
 }
