56207bf700a14dfa77597e96547a5a1ae30786db
Vanessa Tang <vanesst5@uci.edu>
2023-05-09 13:11:55 -0700

    Added AddGenre, AddMovie, AddStar Servlets

---
+++ src/AddGenreServlet.java
@@ -0,0 +1,25 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+@WebServlet(name = "AddGenreServlet", urlPatterns = "/api/add-genre")
+public class AddGenreServlet extends HttpServlet{
+
+}
