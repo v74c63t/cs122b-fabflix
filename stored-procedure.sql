26d2c16e97523f405de8909ea1ef70a18eed95c1
Haver <haverho.2020@gmail.com>
2023-05-09 10:31:46 -0700

    added existence checking as a comment stored-procedure.sql

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -1,9 +1,12 @@
 DELIMITER $$
-CREATE PROCEDURE add_movie (IN title VARCHAR(100), year INT, director VARCHAR(100)) -- add in genre and star info too
+CREATE PROCEDURE add_movie (IN in_title VARCHAR(100), in_year INT, in_director VARCHAR(100), in_star VARCHAR(100), in_genre VARCHAR(32) ) -- add in genre and star info too
 BEGIN
     -- check if movie already exists
+        -- IF EXISTS(SELECT * FROM movies WHERE title = in_title AND year = in_year) THEN
     -- check if star already exists
+        -- IF EXISTS(SELECT * FROM stars WHERE name = in_star) THEN
     -- check if genre already exists
+        -- IF EXISTS(SELECT * FROM genre WHERE name = in_genre) THEN
     -- if not insert them
 #     IF (x > 5) THEN
 #         SELECT CONCAT(x, " is higher") as answer;
