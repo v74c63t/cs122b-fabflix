0b86e30f93376e127b6730048222ffcf430b3331
Vanessa Tang <vanesst5@uci.edu>
2023-05-09 08:50:04 -0700

    Started writing procedure

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -1,1 +1,18 @@
+DELIMITER $$
+CREATE PROCEDURE add_movie (IN title VARCHAR(100), year INT, director VARCHAR(100)) -- add in genre and star info too
+BEGIN
+    -- check if movie already exists
+    -- check if star already exists
+    -- check if genre already exists
+    -- if not insert them
+#     IF (x > 5) THEN
+#         SELECT CONCAT(x, " is higher") as answer;
+#     ELSE
+#         SELECT CONCAT(x, " is lower") as answer;
+#     END IF;
+END
+$$
+-- Change back DELIMITER to ;
+DELIMITER ;
+# SHOW PROCEDURE STATUS WHERE db = 'moviedb';
 
