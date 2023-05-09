8b8d2f0001504af1979e07a45e13090cb775963d
Haver <haverho.2020@gmail.com>
2023-05-09 12:01:10 -0700

    declaring variables to use for add_movie

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -1,6 +1,9 @@
 DELIMITER $$
-CREATE PROCEDURE add_movie (IN in_title VARCHAR(100), in_year INT, in_director VARCHAR(100), in_star VARCHAR(100), in_genre VARCHAR(32) ) -- add in genre and star info too
+CREATE PROCEDURE add_movie (IN in_title VARCHAR(100), in_year INT, in_director VARCHAR(100), in_star VARCHAR(100), in_birth INT, in_genre VARCHAR(32) ) -- add in genre and star info too
 BEGIN
+    DECLARE movieId VARCHAR(10);
+    DECLARE starId VARCHAR(10);
+    DECLARE genreId INT;
     -- check if movie already exists
         -- IF EXISTS(SELECT * FROM movies WHERE title = in_title AND year = in_year) THEN
     -- check if star already exists
