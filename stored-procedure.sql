439565109b57834747d0fde9cd2b561ceab0cfc4
Vanessa Tang <vanesst5@uci.edu>
2023-05-09 12:53:01 -0700

    Added insert statements for stars_in_movies and genres_in_movies

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -18,9 +18,9 @@ INSERT INTO availableInt VALUES(
 DELIMITER $$
 CREATE PROCEDURE add_movie (IN movie_title VARCHAR(100), movie_year INT, movie_director VARCHAR(100), star_name VARCHAR(100), star_birth_year INT, genre_name VARCHAR(32) ) -- add in genre and star info too
 BEGIN
-    DECLARE movieId VARCHAR(10);
-    DECLARE starId VARCHAR(10);
-    DECLARE genreId INT;
+    DECLARE movie_id VARCHAR(10);
+    DECLARE star_id VARCHAR(10);
+    DECLARE genre_id INT;
     -- check if movie already exists
         -- IF EXISTS(SELECT * FROM movies WHERE title = in_title AND year = in_year) THEN
     -- check if star already exists
@@ -31,39 +31,34 @@ BEGIN
         -- send a message saying the movie already exists and end the procedure
     END IF;
 
-    SET movieId = (select concat(substring(max(id), 1,2), (CAST(substring(max(id), 3) AS UNSIGNED) + 1)) from movies);
+    SET movie_id = (select concat(substring(max(id), 1,2), (CAST(substring(max(id), 3) AS UNSIGNED) + 1)) from movies);
 
     -- CHECK STAR
     IF EXISTS(SELECT * FROM stars WHERE name = star_name AND birthYear = star_birth_year) THEN
-		SET starId = (SELECT id FROM stars WHERE name = star_name AND birthYear = star_birth_year);
+		SET star_id = (SELECT id FROM stars WHERE name = star_name AND birthYear = star_birth_year);
     ELSE
         -- parse and increment id
-		SET starId = (SELECT star FROM availableInt);
+		SET star_id = (SELECT star FROM availableInt);
         UPDATE availableInt SET star = star + 1;
-        INSERT INTO stars (id, name, birthYear) VALUES (starId, star_name, star_birth_year);
+        INSERT INTO stars (id, name, birthYear) VALUES (star_id, star_name, star_birth_year);
     END IF;
 
     -- CHECK GENRE
     IF EXISTS(SELECT * FROM genres WHERE name = genre_name) THEN
-		SET genreId = (SELECT id FROM genres WHERE name = genre_name);
+		SET genre_id = (SELECT id FROM genres WHERE name = genre_name);
     ELSE
         -- parse and increment id
 # 		SET genreId = (select max(id) + 1 from genres);
 		-- but its autoincrement so i dont think we need to set genreId?
-        SET genreId = (SELECT genre FROM availableInt);
+        SET genre_id = (SELECT genre FROM availableInt);
         UPDATE availableInt SET genre = genre + 1;
 		INSERT INTO genres (id, name) VALUES (genreId, genre_name);
     END IF;
 
-    INSERT INTO movies (id, title, year, director) VALUES(movieId, movie_title, movie_year, movie_director);
+    INSERT INTO movies (id, title, year, director) VALUES(movie_id, movie_title, movie_year, movie_director);
+    INSERT INTO stars_in_movies(starId, movieId) VALUES (star_id, movie_id);
+    INSERT INTO genres_in_movies(genreId, movieId) VALUES (genre_id, movie_id);
 
-
-
-#     IF (x > 5) THEN
-#         SELECT CONCAT(x, " is higher") as answer;
-#     ELSE
-#         SELECT CONCAT(x, " is lower") as answer;
-#     END IF;
 END
 $$
 -- Change back DELIMITER to ;
