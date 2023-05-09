417286d1034a171a453fb50d4106ff7d57029ae1
Vanessa Tang <vanesst5@uci.edu>
2023-05-09 13:07:08 -0700

    Added message to say if movie was added/already exists

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -22,43 +22,45 @@ BEGIN
     DECLARE star_id VARCHAR(10);
     DECLARE genre_id INT;
     -- check if movie already exists
-        -- IF EXISTS(SELECT * FROM movies WHERE title = in_title AND year = in_year) THEN
+    -- IF EXISTS(SELECT * FROM movies WHERE title = in_title AND year = in_year) THEN
     -- check if star already exists
     -- check if genre already exists
     -- if not insert them
 
     IF EXISTS(SELECT * FROM movies WHERE title = movie_title AND year = movie_year  AND director = movie_director) THEN
         -- send a message saying the movie already exists and end the procedure
-    END IF;
-
-    SET movie_id = (select concat(substring(max(id), 1,2), (CAST(substring(max(id), 3) AS UNSIGNED) + 1)) from movies);
-
-    -- CHECK STAR
-    IF EXISTS(SELECT * FROM stars WHERE name = star_name AND birthYear = star_birth_year) THEN
-		SET star_id = (SELECT id FROM stars WHERE name = star_name AND birthYear = star_birth_year);
+        SELECT CONCAT(movie_title, " already exists") as message;
     ELSE
-        -- parse and increment id
-		SET star_id = (SELECT star FROM availableInt);
-        UPDATE availableInt SET star = star + 1;
-        INSERT INTO stars (id, name, birthYear) VALUES (star_id, star_name, star_birth_year);
+        SET movie_id = (select concat(substring(max(id), 1,2), (CAST(substring(max(id), 3) AS UNSIGNED) + 1)) from movies);
+
+        -- CHECK STAR
+        IF EXISTS(SELECT * FROM stars WHERE name = star_name AND birthYear = star_birth_year) THEN
+            SET star_id = (SELECT id FROM stars WHERE name = star_name AND birthYear = star_birth_year);
+        ELSE
+            -- parse and increment id
+            SET star_id = (SELECT star FROM availableInt);
+            UPDATE availableInt SET star = star + 1;
+            INSERT INTO stars (id, name, birthYear) VALUES (star_id, star_name, star_birth_year);
+        END IF;
+
+        -- CHECK GENRE
+        IF EXISTS(SELECT * FROM genres WHERE name = genre_name) THEN
+            SET genre_id = (SELECT id FROM genres WHERE name = genre_name);
+        ELSE
+            -- parse and increment id
+            # 		SET genreId = (select max(id) + 1 from genres);
+            -- but its autoincrement so i dont think we need to set genreId?
+            SET genre_id = (SELECT genre FROM availableInt);
+            UPDATE availableInt SET genre = genre + 1;
+            INSERT INTO genres (id, name) VALUES (genre_id, genre_name);
+        END IF;
+
+        INSERT INTO movies (id, title, year, director) VALUES(movie_id, movie_title, movie_year, movie_director);
+        INSERT INTO stars_in_movies(starId, movieId) VALUES (star_id, movie_id);
+        INSERT INTO genres_in_movies(genreId, movieId) VALUES (genre_id, movie_id);
+        -- send a message saying movie was successfully added
+        SELECT CONCAT(movie_title, " was succesfully added") as message;
     END IF;
-
-    -- CHECK GENRE
-    IF EXISTS(SELECT * FROM genres WHERE name = genre_name) THEN
-		SET genre_id = (SELECT id FROM genres WHERE name = genre_name);
-    ELSE
-        -- parse and increment id
-# 		SET genreId = (select max(id) + 1 from genres);
-		-- but its autoincrement so i dont think we need to set genreId?
-        SET genre_id = (SELECT genre FROM availableInt);
-        UPDATE availableInt SET genre = genre + 1;
-		INSERT INTO genres (id, name) VALUES (genre_id, genre_name);
-    END IF;
-
-    INSERT INTO movies (id, title, year, director) VALUES(movie_id, movie_title, movie_year, movie_director);
-    INSERT INTO stars_in_movies(starId, movieId) VALUES (star_id, movie_id);
-    INSERT INTO genres_in_movies(genreId, movieId) VALUES (genre_id, movie_id);
-
 END
 $$
 -- Change back DELIMITER to ;
