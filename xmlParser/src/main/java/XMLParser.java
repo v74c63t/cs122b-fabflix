d239f2d2da90b253c0e284a8583d8ef3c11ed164
Vanessa Tang <vanesst5@uci.edu>
2023-05-11 14:40:34 -0700

    Added notes

--- xmlParser/src/main/java/XMLParser.java
+++ xmlParser/src/main/java/XMLParser.java
@@ -1,7 +1,7 @@
 /*
 General Outline:
     - Parse the XML Files
-    - Validate them using in memory hash tables?
+    - Validate them using in memory hash maps?
     - Write them to files (preferably csv files)
         - (one file for each table [movies, stars, genres, stars_in_movies, genres_in_movies])
     - Use SQL LOAD DATA to load each file into the tables
@@ -16,4 +16,13 @@ Some notes abt the XML files:
         - some are all lower case or have upper and lower case characters throughout the name
             - ex: 'susp', 'DRam'
             - might have to use .lower() to check some of these
+ */
+
+/*
+There are dupes within the xml files
+    - have to keep track of stuff to be inserted to check these
+DOM may cause problems with AWS because of memory?
+    - try on AWS to see how long it takes
+    - if too long optimize or switch to SAX
+maybe different parser files for each file? (one for mains, one for casts, one for actors)
  */
