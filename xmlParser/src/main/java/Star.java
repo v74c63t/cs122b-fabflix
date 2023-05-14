8e5f91195afe217d0c4ed4d5e5680b7e11a4ae87
Vanessa Tang <vanesst5@uci.edu>
2023-05-13 17:26:29 -0700

    Added id to Star

--- xmlParser/src/main/java/Star.java
+++ xmlParser/src/main/java/Star.java
@@ -1,6 +1,8 @@
 import java.sql.Date;
 
 public class Star {
+    private String id;
+
     private String name;
 //    private int birthYear; // maybe consider using string for this to deal with nulls
                             // can do checking that it is an int before it is set so it wont be a problem
@@ -10,7 +12,8 @@ public class Star {
     }
 
 //    public Star ( String name, int birthYear ) {
-    public Star ( String name, String birthYear ) {
+    public Star ( String id, String name, String birthYear ) {
+        this.id = id;
         this.name = name;
         this.birthYear = birthYear;
     }
@@ -22,6 +25,14 @@ public class Star {
 
     public String getBirthYear() {
         return birthYear;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     // Setters
@@ -36,6 +47,7 @@ public class Star {
     public String toString() {
         StringBuffer sb = new StringBuffer();
         sb.append("Star Details - ");
+        sb.append("Id:" + getId());
         sb.append("Name:" + getName());
         sb.append(", ");
         sb.append("Birth Year:" + getBirthYear());
