61a15f567e4b6bc1d3a8b7205f710104cb72aa65
Haver Ho <haverho.2020@gmail.com>
2023-05-18 06:54:36 -0700

    modified to get titles using fulltext search

--- src/Autocomplete.java
+++ src/Autocomplete.java
@@ -1,5 +1,10 @@
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.HashMap;
+
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,52 +13,24 @@ import jakarta.servlet.http.HttpServletResponse;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 @WebServlet("/autocomplete")
 public class Autocomplete extends HttpServlet {
-    /*
-     * populate the Super hero hash map.
-     * Key is hero ID. Value is hero name.
-     */
-    public static HashMap<Integer, String> superHeroMap = new HashMap<>();
 
-    static {
-        superHeroMap.put(1, "Blade");
-        superHeroMap.put(2, "Ghost Rider");
-        superHeroMap.put(3, "Luke Cage");
-        superHeroMap.put(4, "Silver Surfer");
-        superHeroMap.put(5, "Beast");
-        superHeroMap.put(6, "Thing");
-        superHeroMap.put(7, "Black Panther");
-        superHeroMap.put(8, "Invisible Woman");
-        superHeroMap.put(9, "Nick Fury");
-        superHeroMap.put(10, "Storm");
-        superHeroMap.put(11, "Iron Man");
-        superHeroMap.put(12, "Professor X");
-        superHeroMap.put(13, "Hulk");
-        superHeroMap.put(14, "Cyclops");
-        superHeroMap.put(15, "Thor");
-        superHeroMap.put(16, "Jean Grey");
-        superHeroMap.put(17, "Wolverine");
-        superHeroMap.put(18, "Daredevil");
-        superHeroMap.put(19, "Captain America");
-        superHeroMap.put(20, "Spider-Man");
-        superHeroMap.put(101, "Superman");
-        superHeroMap.put(102, "Batman");
-        superHeroMap.put(103, "Wonder Woman");
-        superHeroMap.put(104, "Flash");
-        superHeroMap.put(105, "Green Lantern");
-        superHeroMap.put(106, "Catwoman");
-        superHeroMap.put(107, "Nightwing");
-        superHeroMap.put(108, "Captain Marvel");
-        superHeroMap.put(109, "Aquaman");
-        superHeroMap.put(110, "Green Arrow");
-        superHeroMap.put(111, "Martian Manhunter");
-        superHeroMap.put(112, "Batgirl");
-        superHeroMap.put(113, "Supergirl");
-        superHeroMap.put(114, "Black Canary");
-        superHeroMap.put(115, "Hawkgirl");
-        superHeroMap.put(116, "Cyborg");
-        superHeroMap.put(117, "Robin");
+    private static final long serialVersionUID = 1L;
+
+    // Create a dataSource which registered in web.
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
     }
 
     /*
@@ -77,7 +54,7 @@ public class Autocomplete extends HttpServlet {
      *
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
+        try (Connection conn = dataSource.getConnection()) {
             // setup the response json arrray
             JsonArray jsonArray = new JsonArray();
 
@@ -94,12 +71,42 @@ public class Autocomplete extends HttpServlet {
             // this example only does a substring match
             // TODO: in project 4, you should do full text search with MySQL to find the matches on movies and stars
 
-            for (Integer id : superHeroMap.keySet()) {
-                String heroName = superHeroMap.get(id);
-                if (heroName.toLowerCase().contains(query.toLowerCase())) {
-                    jsonArray.add(generateJsonObject(id, heroName));
-                }
+            // Get a list of tokens from input query
+            String[] queries = query.split(" ");
+
+            // Construct a string for each token parameter
+            StringBuffer numQueries = new StringBuffer();
+            for (int i = 0; i < queries.length; ++i) {
+                numQueries.append("? ");
             }
+
+            // Query for full-text search
+                // Not sure about  "*"
+                // Maybe just title if we are only getting title for suggestions
+            String sqlQuery = "SELECT title FROM movies WHERE MATCH(title) AGAINST(" + numQueries + "IN BOOLEAN MODE) LIMIT 10;";
+
+            // Create a statement
+            PreparedStatement statement = conn.prepareStatement(sqlQuery);
+
+            // Set all parameters denoted "?" with associated token
+            for (int i = 0; i < queries.length; ++i) {
+                statement.setString(i+1,"+" + queries[i] + "*");
+            }
+
+            // Execute query
+            ResultSet rs = statement.executeQuery();
+
+            JsonArray jsonArray1 = new JsonArray();
+
+            while ( rs.next() ) {
+                String title = rs.getString("title");
+
+                JsonObject jsonObject = new JsonObject();
+
+                jsonObject.addProperty("title", title);
+                jsonArray.add(jsonObject);
+            }
+
             response.getWriter().write(jsonArray.toString());
         } catch (Exception e) {
             System.out.println(e);
