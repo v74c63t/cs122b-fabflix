06b100bab19ccc150acefb7809185b468e91b087
Vanessa Tang <vanesst5@uci.edu>
2023-05-17 19:47:13 -0700

    Adjusted things

--- create_index.sql
+++ create_index.sql
@@ -1,7 +1,6 @@
 USE moviedb;
 
-CREATE FULLTEXT INDEX titleFullTextIdx
-ON movies(title); -- check if correct before adding
+CREATE FULLTEXT INDEX titleFullTextIdx ON movies(title); -- check if correct before adding
 
 -- create any additional index to make queries faster if need now that there are more records in the db
 
