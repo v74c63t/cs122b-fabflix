d55c81241943e271a0147d376116d0254a78c7c8
Vanessa Tang <vanesst5@uci.edu>
2023-05-23 12:55:17 -0700

    Added back rating

--- Android/app/src/main/java/edu/uci/ics/fabflixmobile/ui/movielist/MovieListViewAdapter.java
+++ Android/app/src/main/java/edu/uci/ics/fabflixmobile/ui/movielist/MovieListViewAdapter.java
@@ -23,6 +23,7 @@ public class MovieListViewAdapter extends ArrayAdapter<Movie> {
         TextView director;
         TextView genres;
         TextView stars;
+        TextView rating;
     }
 
     public MovieListViewAdapter(Context context, ArrayList<Movie> movies) {
@@ -47,6 +48,7 @@ public class MovieListViewAdapter extends ArrayAdapter<Movie> {
             viewHolder.director = convertView.findViewById(R.id.director);
             viewHolder.genres = convertView.findViewById(R.id.genres);
             viewHolder.stars = convertView.findViewById(R.id.stars);
+            viewHolder.rating = convertView.findViewById(R.id.rating);
             // Cache the viewHolder object inside the fresh view
             convertView.setTag(viewHolder);
         } else {
@@ -60,6 +62,7 @@ public class MovieListViewAdapter extends ArrayAdapter<Movie> {
         viewHolder.director.setText("Director: " + movie.getDirector());
         viewHolder.genres.setText("Genres: " + movie.getGenres());
         viewHolder.stars.setText("Stars: " + movie.getStars());
+        viewHolder.rating.setText(movie.getRating());
         // Return the completed view to render on screen
         return convertView;
     }
