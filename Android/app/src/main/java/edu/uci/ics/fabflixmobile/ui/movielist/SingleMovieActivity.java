499f5a7377ed2c9c16646161976346dc0ac048ed
Vanessa Tang <vanesst5@uci.edu>
2023-05-23 15:06:05 -0700

    Created files for single movie

---
+++ Android/app/src/main/java/edu/uci/ics/fabflixmobile/ui/movielist/SingleMovieActivity.java
@@ -0,0 +1,42 @@
+package edu.uci.ics.fabflixmobile.ui.movielist;
+
+import android.annotation.SuppressLint;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import edu.uci.ics.fabflixmobile.R;
+import edu.uci.ics.fabflixmobile.data.model.Movie;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+public class SingleMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_movie);
+        // TODO: this should be retrieved from the backend server
+//        final ArrayList<Movie> movies = new ArrayList<>();
+//        movies.add(new Movie("The Terminal", "2004"));
+//        movies.add(new Movie("The Final Season", "2007"));
+//        Bundle extras = getIntent().getExtras();
+//        String jsonStr = extras.getString("movies");
+//        Gson gson = new Gson();
+//        final ArrayList<Movie> movies = gson.fromJson(jsonStr, new TypeToken<ArrayList<Movie>>(){}.getType());
+//        MovieListViewAdapter adapter = new MovieListViewAdapter(this, movies);
+//        ListView listView = findViewById(R.id.list);
+//        listView.setAdapter(adapter);
+//        listView.setOnItemClickListener((parent, view, position, id) -> {
+//            Movie movie = movies.get(position);
+//            @SuppressLint("DefaultLocale") String message = String.format("Clicked on position: %d, name: %s, %d", position, movie.getTitle(), movie.getYear());
+//            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+//        });
+    }
+}
