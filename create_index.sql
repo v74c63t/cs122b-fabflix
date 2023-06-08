77ceb15b71d826fa1aeb9dba45e2bff466b1559a
Haver Ho <haverho.2020@gmail.com>
2023-06-08 13:55:21 -0700

    added indexes used in FulltextServlet

--- create_index.sql
+++ create_index.sql
@@ -1,3 +1,11 @@
 USE moviedb;
 
 CREATE FULLTEXT INDEX titleFullTextIdx ON movies(title);
+
+CREATE INDEX simMovieIdIdx ON stars_in_movies(movieId);
+
+CREATE INDEX starIdIdx ON stars(id);
+
+CREATE INDEX gimGenreIdIdx ON genres_in_movies(genreId);
+
+CREATE INDEX genreIdIdx ON genres(id);
